4588eba2b27556904e4cfffe249e13908b48f8da
esanchep <esanchezp85@gmail.com>
2021-05-04 23:23:40 +0200

    Add code improvements and clean code

--- index.js
+++ index.js
@@ -1,105 +1,84 @@
-const game = document.getElementById('game')
+const board = document.getElementById('board')
 const WIN_COMBINATIONS = new Set([
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    ['0', '1', '2'],
+    ['3', '4', '5'],
+    ['6', '7', '8'],
+    ['0', '3', '6'],
+    ['1', '4', '7'],
+    ['2', '5', '8'],
+    ['0', '4', '8'],
+    ['2', '4', '6']
 ])
-const CELLS = 9
-let count = 0
+const remainingCells = new Set(['0', '1', '2', '3', '4', '5', '6', '7', '8'])
 const startingPlayer = Math.random() < 0.5 ? 'x' : 'circle'
+const xPlayer = []
+const circlePlayer = []
+const players = new Map([
+    ['x', xPlayer],
+    ['circle', circlePlayer],
+])
+let count = 0
 let activePlayer = startingPlayer
 let shouldToggle = true;
-let xPlayer = []
-let circlePlayer = []
 let winner = undefined
 
 function init() {
     buildGrid()
-    toggleActivePlayer()
+    board.classList.add(startingPlayer)
 }
 
 function buildGrid() {
-    for (let cell = 0; cell < CELLS; cell++) {
-        const newCell = document.createElement('div')
-        newCell.classList.add('cell')
-        newCell.id = count++
-        newCell.addEventListener('click', toggleSelected, { once: true })
-        game.appendChild(newCell)
-    }
+    document
+        .querySelectorAll('.cell')
+        .forEach(cell =>
+            cell.addEventListener('click', toggleSelected, { once: true }))
 }
 
 function toggleSelected($event) {
-    setSelectedCell($event)
-    checkPlayerCells()
+    assignCellToPlayer($event)
+    checkGameState()
     toggleActivePlayer()
 }
 
-function setSelectedCell($event) {
+function assignCellToPlayer($event) {
     $event.target.classList.add('selected')
     $event.target.classList.add(activePlayer)
-    if ($event.target.classList.contains('x')) {
-        xPlayer.push($event.target.id)
-    }
-    if ($event.target.classList.contains('circle')) {
-        circlePlayer.push($event.target.id)
-    }
+    const id = $event.target.id
+    players.get(activePlayer).push(id)
+    remainingCells.delete(id)
 }
 
-function checkPlayerCells() {
-    if (game.classList.contains('x')) {
-        WIN_COMBINATIONS.forEach(combination => {
-            count = 0
-            combination.forEach(id => {
-                if (xPlayer.includes(id.toString())) {
-                    count++
-                    if (count === 3) {
-                        winner = 'xPlayer'
-                    }
+function checkGameState() {
+    WIN_COMBINATIONS.forEach(combination => {
+        count = 0
+        combination.forEach(id => {
+            if (players.get(activePlayer).includes(id)) {
+                count++
+                if (count === 3) {
+                    winner = activePlayer
                 }
-            })
+            }
         })
-    }
-    if (game.classList.contains('circle')) {
-        WIN_COMBINATIONS.forEach(combination => {
-            count = 0
-            combination.forEach(id => {
-                if (circlePlayer.includes(id.toString())) {
-                    count++
-                    if (count === 3) {
-                        winner = 'circlePlayer'
-                    }
-                }
-            })
-        })
-    }
+    })
     if (!!winner) {
         alert(`Player ${winner} wins!`)
         window.location = '/'
     }
-    if (!winner && Array.from(document.getElementsByClassName('cell')).filter(cell => !cell.classList.contains('selected')).length === 0) {
+    if (!winner && remainingCells.size === 0) {
         alert(`Draw!`)
         window.location = '/'
     }
 }
 
 function toggleActivePlayer() {
-    if (!(game.classList.contains('x') || game.classList.contains('circle'))) {
-        game.classList.add(startingPlayer)
-    }
-    if (game.classList.contains('x')) {
-        game.classList.replace('x', 'circle');
+    if (activePlayer === 'x') {
         activePlayer = 'circle'
+        board.classList.replace('x', 'circle');
         return;
     }
-    if (game.classList.contains('circle')) {
-        game.classList.replace('circle', 'x');
+    if (activePlayer === 'circle') {
         activePlayer = 'x'
-        return;
+        board.classList.replace('circle', 'x');
     }
 }
 
